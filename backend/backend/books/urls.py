ebe1c1c721185bd13d35547c5c0514d027419d9c
AymenLaroussi <larousi.aymen@gmail.com>
2025-07-01 17:06:00 +0200

    Retrieve Books List

--- backend/backend/books/urls.py
+++ backend/backend/books/urls.py
@@ -1,7 +1,8 @@
 # backend/books/urls.py
 from django.urls import path
-from .views import createBook
+from .views import createBook,retrieveBook
 
 urlpatterns = [
     path('books/', createBook.as_view(), name='book-list-create'),
+    path('books/', retrieveBook.as_view(), name='book-list'),
 ]
